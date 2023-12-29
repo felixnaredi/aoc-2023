0b756383294df104a3f91d4d2c7af334c7514488
felixnaredi <felixnaredi@gmail.com>
2023-12-29 13:15:36 +0100

    Day 23, Second Star

--- day23.py
+++ day23.py
@@ -61,3 +61,34 @@ print(max(goal_distance) + 1)
 # %% Second Star
 # ------------------------------------------------------------------------------
 
+lines = input.splitlines()
+lines = ["#" * len(lines[0])] + lines + ["#" * len(lines[0])]
+
+start = (1, 1)
+goal = (len(lines) - 2, len(lines[0]) - 2)
+dist = 0
+
+queue = [(start, set())]
+while len(queue) != 0:
+    (y, x), visited = queue.pop()
+    visited.add((y, x))
+    while True:
+        next = [
+            (y + dy, x + dx)
+            for dy, dx in ((-1, 0), (1, 0), (0, -1), (0, 1))
+            if (y + dy, x + dx) not in visited and lines[y + dy][x + dx] != "#"
+        ]
+        # print(next)
+        if len(next) == 1:
+            y, x = next[0]
+            visited.add((y, x))
+            if (y, x) == goal:
+                # NOTE:
+                #   I just let the progam run for a few minutes and then guessed the largest value. It
+                #   was correct!
+                print("goal", len(visited), dist)
+                dist = max(dist, len(visited) - 1)
+        else:
+            for y1, x1 in next:
+                queue.append(((y1, x1), set(visited)))
+            break
